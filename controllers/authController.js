5c324b3d588d6398db089b1c4d64a72ad7ab9855
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2023-08-14 11:41:46 -0300

    feat:  fixed controller done

--- controllers/authController.js
+++ controllers/authController.js
@@ -87,44 +87,38 @@ const login = async (req, res) => {
 const forgotPassword = async (req, res) => {
     try {
         const { email } = req.body;
-
         // Verificar si el usuario existe en la base de datos
         const user = await User.findOne({ email });
         if (!user) {
             throw createError(400, "El usuario con este correo electrónico no existe.");
         }
-
         // Generar un token de recuperación de contraseña (por ejemplo, un token aleatorio)
         const resetToken = generateTokenRandom();
-
         // Guardar el token de recuperación en el usuario
         user.resetPasswordToken = resetToken;
         user.resetPasswordExpires = Date.now() + 3600000; // Expira en 1 hora
-
         await user.save();
-
         // Configurar el transporte de Nodemailer con la configuración adecuada
         const transporter = nodemailer.createTransport({
             service: 'Gmail',
             auth: {
-              user: process.env.EMAIL_USER, // Utiliza la variable de entorno EMAIL_USER
-              pass: process.env.EMAIL_PASSWORD // Utiliza la variable de entorno EMAIL_PASSWORD
+                user: process.env.EMAIL_USER, // Utiliza la variable de entorno EMAIL_USER
+                pass: process.env.EMAIL_PASSWORD // Utiliza la variable de entorno EMAIL_PASSWORD
             }
-          });
-
+        });
+        // Genera el enlace completo con el token en la URL
+        const resetLink = `https://app-client-q8i7.vercel.app/update-password/${resetToken}`;
         // Configurar el contenido del correo electrónico
         const mailOptions = {
             from: process.env.EMAIL_USER, // Tu dirección de correo electrónico
             to: email, // El correo electrónico del usuario que solicitó la recuperación de contraseña
             subject: 'Recuperación de contraseña',
-            text: `Haz clic en el siguiente enlace para restablecer tu contraseña: http://localhost:3000/reset-password?token=${resetToken}`,
+            text: `Haz clic en el siguiente enlace para restablecer tu contraseña: ${resetLink}`,
             // Puedes personalizar el contenido del correo electrónico según tus necesidades.
             // También puedes enviar un correo electrónico en formato HTML.
         };
-
         // Envía el correo electrónico
         await transporter.sendMail(mailOptions);
-
         return res.status(200).json({
             ok: true,
             message: "Se ha enviado un correo electrónico con las instrucciones para restablecer la contraseña.",
@@ -136,9 +130,36 @@ const forgotPassword = async (req, res) => {
         });
     }
 };
+const updatePassword = async (req, res) => {
+    try {
+        const { token, password } = req.body;
+        // Verificar si el usuario existe en la base de datos a través del token
+        const user = await User.findOne({ resetPasswordToken: token, resetPasswordExpires: { $gt: Date.now() } });
+        if (!user) {
+            throw createError(400, "El token de restablecimiento de contraseña no es válido o ha expirado.");
+        }
+        // Restablecer la contraseña del usuario y eliminar el token
+        user.password = password;
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpires = undefined;
+        await user.save();
+        return res.status(200).json({
+            ok: true,
+            message: "Contraseña restablecida exitosamente.",
+        });
+    } catch (error) {
+        return res.status(error.status || 500).json({
+            ok: false,
+            message: error.message || "Upss, hubo un error al restablecer la contraseña.",
+        });
+    }
+};
+
+
 module.exports = {
     register,
     login,
-    forgotPassword, // Agrega el nuevo endpoint aquí
+    forgotPassword,
+    updatePassword, // Agrega el nuevo endpoint aquí
 };
 
--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const { register, login, forgotPassword } = require('../controllers/authController');
+const { register, login, forgotPassword,updatePassword } = require('../controllers/authController');
 const { profile, toggleFavorite } = require('../controllers/userController');
 const checkToken = require('../middlewares/checkToken');
 
@@ -14,6 +14,7 @@ router
   .post('/api/register', register)
   .post('/api/login', login)
   .post('/api/forgot-password',forgotPassword)
+  .post('/api/update-password', updatePassword)
   .get('/api/profile', checkToken, profile)
   .get('/api/favorite', checkToken, toggleFavorite)
 module.exports = router;
